2b00a5993779d22aee26fae10d33a4aa9605466b
Sebastian Rettig <serettig@posteo.de>
2022-01-22 16:29:41 +0100

    refactor: code cleanup

--- src/index.ts
+++ src/index.ts
@@ -1,34 +1,66 @@
 import express from "express";
 import * as wpAuth from "wp-auth";
-import { promisify } from "util";
+import debug from "debug";
 
-import { getUserData, hasCap, loadRoles } from "./wp-sql";
+import Settings from "./Settings";
+import WordPressDB from "./WordPressDB";
+
+
+const log = debug("wp-microservice");
+let db: WordPressDB;
+let settings: Settings;
 
 const start = async () => {
-  await loadRoles();
+  // Get settings (from environment variables)
+  settings = Settings.load();
+  log("Settings loaded");
+
+  // Set up database repository
+  db = new WordPressDB(
+    settings.dbHost,
+    settings.dbUser,
+    settings.dbPassword,
+    settings.dbName
+  );
+  // Load roles from WordPress database to allow checks
+  try {
+    await db.init();
+  } catch (error) {
+    console.error(error);
+    // Terminate with non-zero error code
+    process.exit(1);
+  }
+  log("Initialized database");
 
   const app = express();
-  const port = 3000;
 
   const authenticator = wpAuth.create({
-    wpurl: "http://localhost:7000",
-    logged_in_key: "d86935b67dd417fa2347bfa7972763be3c1a845d",
-    logged_in_salt: "80564189220e440ff813ca05883cb8f220881034",
-    mysql_host: "localhost",
-    mysql_user: "wordpress",
-    mysql_pass: "wordpress",
-    mysql_port: "3306",
-    mysql_db: "wordpress",
-    wp_table_prefix: "wp_",
+    wpurl: settings.wordpressUrl,
+    logged_in_key: settings.loggedInKey,
+    logged_in_salt: settings.loggedInSalt,
+    mysql_host: settings.dbHost,
+    mysql_user: settings.dbUser,
+    mysql_pass: settings.dbPassword,
+    mysql_db: settings.dbName,
+    mysql_port: settings.dbPort,
+    wp_table_prefix: settings.tablePrefix,
   });
+  log("WordPress authenticator initialized");
 
   app.use((req: express.Request, res, next): void => {
     authenticator
       .checkAuth(req)
       .on("auth", async (authIsValid: boolean, userId: string | undefined) => {
-        console.log("Received authentication information from WP");
+        log("Received authentication information from WP");
         if (authIsValid && userId) {
-          const data = await getUserData(userId);
+          const data = await db.getUserData(userId);
+          if (!data) {
+            log("User does not exist in user table. Redirecting to login site");
+            res.redirect(
+              `${settings.wordpressUrl}/wp-login.php?redirect_to=${settings.microserviceUrl}`
+            );
+            return;
+          }
           req.user = { ...data, id: userId };
 
           const roles: any = await new Promise((resolve, reject) => {
@@ -44,16 +76,24 @@ const start = async () => {
             if (roles[role] !== true) {
               continue;
             }
-            if (hasCap(role, "edit_posts")) {
+            if (db.hasCapability(role, "edit_posts")) {
               req.user.permission = "privileged";
             }
           }
           if (!req.user?.permission) {
             req.user.permission = "user";
           }
+          log(
+            "User is logged in with permission level %s",
+            req.user.permission
+          );
           next();
         } else {
-          res.redirect(`http://localhost:7000/wp-login.php?redirect_to=http://localhost:${port}`)
+          log("User is not logged in. Redirecting to login site");
+          res.redirect(
+            `${settings.wordpressUrl}/wp-login.php?redirect_to=${settings.microserviceUrl}`
+          );
+          return;
         }
       });
   });
@@ -69,8 +109,8 @@ const start = async () => {
     res.send(output);
   });
 
-  app.listen(port, () => {
-    console.log(`Example app listening at http://localhost:${port}`);
+  app.listen(settings.port, () => {
+    console.log(`Microservice listening at http://localhost:${settings.port}`);
   });
 };
 
